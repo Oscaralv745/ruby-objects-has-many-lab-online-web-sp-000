4c12e3ce217b1795ba339c2046975f4b4ac67c64
Oscar Alvarez <gavinalv6@gmail.com>
2021-06-14 02:52:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -4,4 +4,13 @@ class Author
   def initialize(name)
     @name = name
   end
+
+  def add_post(post)
+    @post << post
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
 end
